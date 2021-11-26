112006277f1492430633a246ad645cce85e2d872
jfs <juanschiavi48@gmail.com>
2021-11-26 17:26:28 -0300

    Agregue pin interrupt (sin probar).

--- main/i2c_example_main.c
+++ main/i2c_example_main.c
@@ -15,6 +15,11 @@
 #include "esp_log.h"
 #include "driver/i2c.h"
 
+//interrupt
+#include <stdlib.h>
+#include "driver/gpio.h"
+//interrupt
+
 //mqtt headers
 #include <stdint.h>
 #include <stddef.h>
@@ -81,6 +86,41 @@ static const char *TAG = "i2c-example";
 #define NACK_VAL 0x1                            /*!< I2C nack value */
 
 SemaphoreHandle_t print_mux = NULL;
+
+
+
+
+
+#define GPIO_OUTPUT_IO_0    4
+//#define GPIO_OUTPUT_IO_1    19
+#define GPIO_OUTPUT_PIN_SEL  ( 1ULL<<GPIO_OUTPUT_IO_0 )
+#define GPIO_INPUT_IO_0     5
+//#define GPIO_INPUT_IO_1     5
+#define GPIO_INPUT_PIN_SEL  ( 1ULL<<GPIO_INPUT_IO_0 )
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+        }
+    }
+}
+
+
+
+
+
 
 /**
  * @brief test code to read esp-i2c-slave
@@ -470,6 +510,53 @@ void app_main(void)
 
     xTaskCreate(vLevelMeasureTask, "vLevelMeasureTask_0", 1024 * 8, (void *)0, 10, NULL);
     //xTaskCreate(vLevelMeasureTask, "vLevelMeasureTask_1", 1024 * 2, (void *)1, 10, NULL);
+
+
+    // PIN INTERRUPT
+
+    // pin como salida para interrumpir por software al pin que esta como entrada:
+    gpio_config_t io_conf;
+	//disable interrupt
+	io_conf.intr_type = GPIO_INTR_DISABLE;
+	//set as output mode
+	io_conf.mode = GPIO_MODE_OUTPUT;
+	//bit mask of the pins that you want to set,e.g.GPIO18/19
+	io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+	//disable pull-down mode
+	io_conf.pull_down_en = 0;
+	//disable pull-up mode
+	io_conf.pull_up_en = 0;
+	//configure GPIO with the given settings
+	gpio_config(&io_conf);
+
+
+	// pin como entrada, interrupcion:
+	//interrupt of any edge
+	io_conf.intr_type = GPIO_INTR_ANYEDGE;
+	//bit mask of the pins, use GPIO5 here
+	io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+	//set as input mode
+	io_conf.mode = GPIO_MODE_INPUT;
+	//enable pull-up mode
+	io_conf.pull_up_en = 1;
+	gpio_config(&io_conf);
+
+
+	//change gpio intrrupt type for one pin
+	//gpio_set_intr_type(GPIO_INPUT_IO_0, GPIO_INTR_ANYEDGE);
+
+	//create a queue to handle gpio event from isr
+	gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+	//start gpio task
+	xTaskCreate(gpio_task, "gpio_task_example", 2048, NULL, 10, NULL);
+
+	//install gpio isr service
+	gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+	//hook isr handler for specific gpio pin
+	gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void*) GPIO_INPUT_IO_0);
+
+    // PIN INTERRUPT
+
 
 
 }
